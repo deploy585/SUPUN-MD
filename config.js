8aa78d47ac1fb8e6a8cdbc079869a2a36c699343
deploy585 <deploydeploy995@gmail.com>
2025-01-28 01:04:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'session id', //add your session id
-MONGODB: process.env.MONGODB || "mongodb url", //add your mongodb url
+SESSION_ID: process.env.SESSION_ID || 'gtRUgQhB#PLMsTTUlj2YAioYxpdkEnBNnmDwPo0qaQXev3LsuA8wftrc', //add your session id
+MONGODB: process.env.MONGODB || "mongodb+srv://athulakumara604:qBwvqo6IM64eT1SL@cluster0.wr7rx.mongodb.net/", //add your mongodb url
  URL : process.env.BOT_MENU_LINKS || 'https://i.ibb.co/bHXBV08/9242c844b83f7bf9.jpg',
 MAX_SIZE: 500
 };
